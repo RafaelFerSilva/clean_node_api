eef930af8cc590c95d7a0aff11fe46e3fea679cb
RafaelFerSilva <todosomtodavoz@gmail.com>
2026-05-13 22:17:38 -0300

    chore: add jest

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,10 @@ import love from 'eslint-config-love'
 
 export default [
     {
+        ignores: ['node_modules/**', 'dist/**', 'coverage/**'],
+    },
+    {
         ...love,
-        files: ['**/*.js', '**/*.ts', '**/*.tsx'],
+        files: ['**/*.js', '**/*.cjs', '**/*.ts', '**/*.tsx'],
     },
 ]
