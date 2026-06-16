21538093214ff98e986bacace1996cd8e0b47ef8
RafaelFerSilva <todosomtodavoz@gmail.com>
2026-06-16 08:08:06 -0300

    chore: eslint and prettier config

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
 import love from 'eslint-config-love'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default [
     {
@@ -7,5 +8,29 @@ export default [
     {
         ...love,
         files: ['**/*.js', '**/*.cjs', '**/*.ts', '**/*.tsx'],
+        rules: {
+            ...love.rules,
+            '@typescript-eslint/strict-boolean-expressions': 'off',
+            '@typescript-eslint/no-magic-numbers': 'off',
+            '@typescript-eslint/class-methods-use-this': 'off',
+            '@typescript-eslint/no-unsafe-type-assertion': 'off',
+        },
     },
+    {
+        files: ['**/*.spec.ts', '**/*.test.ts'],
+        languageOptions: {
+            globals: {
+                describe: 'readonly',
+                test: 'readonly',
+                expect: 'readonly',
+                it: 'readonly',
+                beforeEach: 'readonly',
+                afterEach: 'readonly',
+                beforeAll: 'readonly',
+                afterAll: 'readonly',
+                jest: 'readonly',
+            },
+        },
+    },
+    eslintConfigPrettier,
 ]
--- jest.config.cjs
+++ jest.config.cjs
@@ -1,20 +1,23 @@
 'use strict'
 
 module.exports = {
-  roots: ['<rootDir>/src'],
-  collectCoverage: true,
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coverageDirectory: "coverage",
-  testEnvironment: "node",
-  extensionsToTreatAsEsm: ['.ts'],
-  transform: {
-    '^.+\\.ts$': ['ts-jest', {
-      useESM: true,
-      tsconfig: 'tsconfig.jest.json'
-    }]
-  },
-  moduleNameMapper: {
-    '^(\\.\\.\?\\/.+)\\.ts$': '$1',
-    '^(\\.\\.\?\\/.+)\\.js$': '$1'
-  }
+    roots: ['<rootDir>/src'],
+    collectCoverage: true,
+    collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+    coverageDirectory: 'coverage',
+    testEnvironment: 'node',
+    extensionsToTreatAsEsm: ['.ts'],
+    transform: {
+        '^.+\\.ts$': [
+            'ts-jest',
+            {
+                useESM: true,
+                tsconfig: 'tsconfig.jest.json',
+            },
+        ],
+    },
+    moduleNameMapper: {
+        '^(\\.\\.?\\/.+)\\.ts$': '$1',
+        '^(\\.\\.?\\/.+)\\.js$': '$1',
+    },
 }
