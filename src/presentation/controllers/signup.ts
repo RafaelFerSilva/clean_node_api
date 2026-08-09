5dcd0b477e9ff16232ee9ef1cc22b56fee295c98
RafaelFerSilva <todosomtodavoz@gmail.com>
2026-08-09 16:03:20 -0300

    feat: Should return 500 if emailValidator throws

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,29 +4,37 @@ import { badRequest } from '../helpers/http-helper.ts'
 import type { Controller } from '../protocols/controller.ts'
 import type { EmailValidator } from '../protocols/email-validator.ts'
 import { InvalidParamError } from '../errors/invalid-param-error.ts'
+import { ServerError } from '../errors/server-error.ts'
 
 export class SignUpController implements Controller {
     constructor(private readonly emailValidator: EmailValidator) {}
 
     handle(httpRequest: HttpRequest): HttpResponse {
-        const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
-        const body = httpRequest.body as Record<string, unknown>
+        try {
+            const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+            const body = httpRequest.body as Record<string, unknown>
 
-        for (const field of requiredFields) {
-            if (!body[field]) {
-                return badRequest(new MissingParamError(field))
+            for (const field of requiredFields) {
+                if (!body[field]) {
+                    return badRequest(new MissingParamError(field))
+                }
             }
-        }
 
-        const { email } = body
-        const isValid = this.emailValidator.isValid(email as string)
-        if (!isValid) {
-            return badRequest(new InvalidParamError('email'))
-        }
+            const { email } = body
+            const isValid = this.emailValidator.isValid(email as string)
+            if (!isValid) {
+                return badRequest(new InvalidParamError('email'))
+            }
 
-        return {
-            statusCode: 200,
-            body: {},
+            return {
+                statusCode: 200,
+                body: {},
+            }
+        } catch (error) {
+            return {
+                statusCode: 500,
+                body: new ServerError(),
+            }
         }
     }
 }
