fa0543a862f1c12dfa407584c134502797168e18
RafaelFerSilva <todosomtodavoz@gmail.com>
2026-08-17 14:59:42 -0300

    feat: implement and register CORS middleware with integration test

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,8 @@
 import type { Express } from 'express'
 import { bodyParser } from '../middlewares/body-parser.js'
+import { cors } from '../middlewares/cors.js'
 
 export default (app: Express): void => {
     app.use(bodyParser)
+    app.use(cors)
 }
---
+++ src/main/middlewares/cors.ts
@@ -0,0 +1,8 @@
+import type { Request, Response, NextFunction } from 'express'
+
+export const cors = (_req: Request, res: Response, next: NextFunction): void => {
+    res.header('access-control-allow-origin', '*')
+    res.header('access-control-allow-methods', '*')
+    res.header('access-control-allow-headers', '*')
+    next()
+}
