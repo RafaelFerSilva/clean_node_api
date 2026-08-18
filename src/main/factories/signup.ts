d1e0c435edd206b769666bd5f36a894b0453227a
RafaelFerSilva <todosomtodavoz@gmail.com>
2026-08-18 17:24:38 -0300

    feat: implement LogControllerDecorator and wrap the SignUpController factory with it

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -3,12 +3,15 @@ import { EmailValidatorAdapter } from '../../utils/email-validator-adapter.ts'
 import { DbAddAccount } from '../../data/usercases/add-account/db-add-account.ts'
 import { BcryptAdapter } from '../../infra/criptography/bcript-dapter.ts'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account.ts'
+import type { Controller } from '../../presentation/controllers/signup/signup-protocols.ts'
+import { LogControllerDecorator } from '../decorators/log.ts'
 
-export const makeSignUpController = (): SignUpController => {
+export const makeSignUpController = (): Controller => {
     const emailValidatorAdapter = new EmailValidatorAdapter()
     const salt = 12
     const bcryptAdapter = new BcryptAdapter(salt)
     const accountMongoRepository = new AccountMongoRepository()
     const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
-    return new SignUpController(emailValidatorAdapter, dbAddAccount)
+    const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount)
+    return new LogControllerDecorator(signUpController)
 }
