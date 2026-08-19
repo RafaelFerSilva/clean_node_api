1918299f5d7f3701510783d4af44a46c1c1c7a74
RafaelFerSilva <todosomtodavoz@gmail.com>
2026-08-19 15:16:54 -0300

    feat: implement LogErrorRepository to log server errors in LogControllerDecorator

--- src/main/decorators/log.ts
+++ src/main/decorators/log.ts
@@ -1,3 +1,4 @@
+import type { LogErrorRepository } from '../../data/protocols/log-error-repository.ts'
 import type {
     Controller,
     HttpRequest,
@@ -5,10 +6,18 @@ import type {
 } from '../../presentation/controllers/signup/signup-protocols.ts'
 
 export class LogControllerDecorator implements Controller {
-    constructor(private readonly controller: Controller) {}
+    constructor(
+        private readonly controller: Controller,
+        private readonly logErrorRepository: LogErrorRepository,
+    ) {}
 
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
         const httpResponse = await this.controller.handle(httpRequest)
+
+        if (httpResponse.statusCode === 500) {
+            const error = httpResponse.body as Error
+            await this.logErrorRepository.logError(error.stack ?? '')
+        }
 
         return httpResponse
     }
