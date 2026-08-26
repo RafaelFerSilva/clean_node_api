c66315777be5e8ace0cda286023571c765e90d4d
RafaelFerSilva <todosomtodavoz@gmail.com>
2026-08-26 08:36:42 -0300

    test: add unit test for validation error handling in signup controller

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -186,4 +186,14 @@ describe('SignUp Controller', () => {
         await sut.handle(httpRequest)
         expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
     })
+
+    test('Should return 400 if validation returns an error', async () => {
+        const { sut, validationStub } = makeSut()
+        const error = new MissingParamError('any_field')
+        jest.spyOn(validationStub, 'validate').mockReturnValueOnce(error)
+
+        const httpRequest = makeFakeRequest()
+        const httpResponse = await sut.handle(httpRequest)
+        expect(httpResponse).toEqual(badRequest(error))
+    })
 })
