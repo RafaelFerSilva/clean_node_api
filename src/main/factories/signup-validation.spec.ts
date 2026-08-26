e74801f35d5aec874b8d5f815816fd2b3053544e
RafaelFerSilva <todosomtodavoz@gmail.com>
2026-08-26 10:31:48 -0300

    test: add unit tests for SignUpValidation factory composition

---
+++ src/main/factories/signup-validation.spec.ts
@@ -0,0 +1,16 @@
+import { makeSignUpValidation } from './signup-validation.ts'
+import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite.ts'
+import { RequiredFieldValidation } from '../../presentation/helpers/validators/required-field-validation.ts'
+
+jest.mock('../../presentation/helpers/validators/validation-composite.ts')
+
+describe('SignUpValidationFactory', () => {
+    it('Should call ValidationComposite with all validations', () => {
+        makeSignUpValidation()
+        const validations = []
+        for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
+            validations.push(new RequiredFieldValidation(field))
+        }
+        expect(ValidationComposite).toHaveBeenCalledWith(validations)
+    })
+})
