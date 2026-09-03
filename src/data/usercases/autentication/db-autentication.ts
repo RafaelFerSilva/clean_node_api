509591b5fff1940cceadf0c444d60aa326da7470
RafaelFerSilva <todosomtodavoz@gmail.com>
2026-09-02 21:50:32 -0300

    feat: implement password comparison using HashComparer in DBAutenticationUseCase

--- src/data/usercases/autentication/db-autentication.ts
+++ src/data/usercases/autentication/db-autentication.ts
@@ -4,12 +4,27 @@ import type {
     AutenticationModel,
 } from '../../../domain/usecases/autentication.js'
 import type { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository.ts'
+import type { HashComparer } from '../../protocols/criptoghraphy/has-comparer.ts'
 
 export class DBAutenticationUseCase implements Autentication {
-    constructor(private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository) {}
+    constructor(
+        private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
+        private readonly hashComparer: HashComparer,
+    ) {}
 
     async auth(autenticationParams: AutenticationParams): Promise<AutenticationModel | null> {
-        await this.loadAccountByEmailRepository.loadByEmail(autenticationParams.email)
+        const account = await this.loadAccountByEmailRepository.loadByEmail(
+            autenticationParams.email,
+        )
+        if (account) {
+            const isValid = await this.hashComparer.compare(
+                autenticationParams.password,
+                account.password,
+            )
+            if (isValid) {
+                return null
+            }
+        }
         return null
     }
 }
