767aa5d4dc69edf7103c11035a9af9e3bda4bb65
RafaelFerSilva <todosomtodavoz@gmail.com>
2026-09-02 22:15:08 -0300

    feat: inject UpdateAccessTokenRepository to update user token upon successful authentication

--- src/data/usercases/autentication/db-autentication.ts
+++ src/data/usercases/autentication/db-autentication.ts
@@ -6,12 +6,14 @@ import type {
 import type { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository.ts'
 import type { HashComparer } from '../../protocols/criptoghraphy/has-comparer.ts'
 import type { TokenGenerator } from '../../protocols/criptoghraphy/token-generator.ts'
+import type { UpdateAccessTokenRepository } from '../../protocols/db/update-access-token-repository.ts'
 
 export class DBAutenticationUseCase implements Autentication {
     constructor(
         private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
         private readonly hashComparer: HashComparer,
         private readonly tokenGenerator: TokenGenerator,
+        private readonly updateAccessTokenRepository: UpdateAccessTokenRepository,
     ) {}
 
     async auth(autenticationParams: AutenticationParams): Promise<AutenticationModel | null> {
@@ -25,6 +27,7 @@ export class DBAutenticationUseCase implements Autentication {
             )
             if (isValid) {
                 const accessToken = await this.tokenGenerator.generate(account.id)
+                await this.updateAccessTokenRepository.update(account.id, accessToken)
                 return { accessToken }
             }
         }
